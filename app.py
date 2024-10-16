59cc2c2b940c2098679c9586447c3f6be265e422
Wayne5421 <caio.gabrielpdn@gmail.com>
2024-10-16 20:35:24 -0300

    [Caio] (chore) - update full backend

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-
 import pandas as pd
 import psycopg2
 import json
-
 
 DB_CONFIG = {
     'host': 'brashly-intent-urchin.data-1.use1.tembo.io',
@@ -12,48 +10,224 @@ DB_CONFIG = {
     'port': 5432
 }
 
-def fetch_data():
+def get_connection():
+    return psycopg2.connect(**DB_CONFIG)
+
+#----------------------------------------------------------------------------------------
+
+def remove_duplicates(conn):
+    with conn.cursor() as cursor:
+        cursor.execute("""
+        DELETE FROM clientes
+        WHERE id NOT IN (
+            SELECT MIN(id) 
+            FROM clientes 
+            GROUP BY nome, sobrenome, telefone, email
+        );
+        """)
 
 
-        conn = psycopg2.connect(**DB_CONFIG)
-        query = f"""
-        
-            SELECT 
-            c.nome AS cliente_nome,
-            c.sobrenome AS cliente_sobrenome,
-            d.nome AS dentista_nome,
-            a.service,
-            a.status,
-            a.date
-        FROM 
-            agendamentos a
-        JOIN 
-            clientes c ON a.id_cliente = c.id
-        JOIN 
-            dentistas d ON a.id_dentista = d.id;
+#----------------------------------------------------------------------------------------
 
+def create_client(nome_cliente, sobrenome_cliente, telefone, email):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        create_cliente = """
+        INSERT INTO clientes (nome, sobrenome, telefone, email) 
+        VALUES (%s, %s, %s, %s);
         """
-    
+        cursor.execute(create_cliente, (nome_cliente, sobrenome_cliente, telefone, email))
+        remove_duplicates(conn)
+        conn.commit()
 
-        df = pd.read_sql_query(query, conn)
-        
+#----------------------------------------------------------------------------------------
 
+
+def create_agendamento(cracha, data, service, status):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        create_agendamento = """
+        INSERT INTO agendamentos (cracha, data, service, status) 
+        VALUES (%s, %s, %s, %s);
+        """
+        cursor.execute(create_agendamento, (cracha, data, service, status))
+        remove_duplicates(conn)
+        conn.commit()
+
+#----------------------------------------------------------------------------------------
+
+def create_dentista(cracha, nome, sobrenome, especialidade, telefone, email):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        create_dentista = """
+        INSERT INTO dentistas (cracha, nome, sobrenome, especialidade, telefone, email) 
+        VALUES (%s, %s, %s, %s, %s, %s);
+        """
+        cursor.execute(create_dentista, (cracha, nome, sobrenome, especialidade, telefone, email))
+        remove_duplicates(conn)
+        conn.commit()
+
+#----------------------------------------------------------------------------------------
+
+def read_data(tabela, id):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        read_data_query = f"""
+            SELECT * 
+            FROM {tabela}
+            ORDER BY {id};
+        """
+        try:
+            cursor.execute(read_data_query)
+            columns = [desc[0] for desc in cursor.description]
+            data = cursor.fetchall()
+            return [dict(zip(columns, row)) for row in data]
+                
+        except Exception as e:
+            print('Erro:', e)
+            return []
+        finally:
+            conn.close()
+#----------------------------------------------------------------------------------------
+
+
+def update_cliente(id_cliente, novo_nome, novo_sobrenome, novo_telefone, novo_email):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        update_query = f"""
+        UPDATE clientes
+        SET nome = %s, 
+            sobrenome = %s, 
+            telefone = %s, 
+            email = %s
+        WHERE id = %s;
+        """
+        cursor.execute(update_query, (novo_nome, novo_sobrenome, novo_telefone, novo_email, id_cliente))
+        conn.commit()
+#----------------------------------------------------------------------------------------
+
+def update_dentista(cracha, novo_nome, novo_sobrenome, nova_especialidade, novo_telefone, novo_email):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        update_query = """
+        UPDATE dentistas
+        SET nome = %s, 
+            sobrenome = %s, 
+            especialidade = %s, 
+            telefone = %s, 
+            email = %s
+        WHERE cracha = %s;
+        """
+        cursor.execute(update_query, (novo_nome, novo_sobrenome, nova_especialidade, novo_telefone, novo_email, cracha))
+        conn.commit()
+
+#----------------------------------------------------------------------------------------
+
+
+def update_agendamento(id_agendamento, novo_cracha, nova_data, novo_servico, novo_status):
+    conn = get_connection()
+    try:
+        with conn.cursor() as cursor:
+            update_query = """
+            UPDATE agendamentos
+            SET cracha = %s, 
+                data = %s, 
+                service = %s, 
+                status = %s
+            WHERE id = %s;
+            """
+            cursor.execute(update_query, (novo_cracha, nova_data, novo_servico, novo_status, id_agendamento))
+            conn.commit()
+            print(f"Agendamento com ID {id_agendamento} atualizado com sucesso.")
+    except Exception as e:
+        print(f"Erro ao atualizar agendamento: {e}")
+    finally:
         conn.close()
+
+
+
+#----------------------------------------------------------------------------------------
+def delete_client(id_cliente):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        delete_query = """
+        DELETE FROM clientes
+        WHERE id = %s;
+        """
+        cursor.execute(delete_query, (id_cliente,))
+        print(f"Cliente com ID {id_cliente} foi deletado.")
+        conn.commit()
         
-        return df
+#----------------------------------------------------------------------------------------
+
+def delete_agendamento(id_agendamento):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        delete_query = """
+        DELETE FROM agendamentos
+        WHERE id = %s;
+        """
+        cursor.execute(delete_query, (id_agendamento,))
+        print(f"Agendamento com ID {id_agendamento} foi deletado.")
+        conn.commit()
+
+#----------------------------------------------------------------------------------------
 
 
-def generate_json():
-    df = fetch_data()
-    
-    if df is not None:
+def delete_dentista(cracha):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        delete_query = """
+        DELETE FROM dentistas
+        WHERE cracha = %s;
+        """
+        cursor.execute(delete_query, (cracha,))
+        print(f"Dentista com crachá {cracha} foi deletado.")
+        conn.commit()
 
-        result = df.to_json(orient='records')
+#----------------------------------------------------------------------------------------
+
+
+def renumber_ids():
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        cursor.execute("""
+        WITH numbered_clients AS (
+            SELECT id, ROW_NUMBER() OVER (ORDER BY id) AS new_id
+            FROM clientes
+        )
+        UPDATE clientes
+        SET id = new_id
+        FROM numbered_clients
+        WHERE clientes.id = numbered_clients.id;
+        """)
+        print("IDs foram renumerados.")
+        conn.commit()
+
+#----------------------------------------------------------------------------------------
+
+
+def renumber_ids_agendamentos():
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        cursor.execute(""" 
+        WITH numbered_agendamentos AS (
+            SELECT id, ROW_NUMBER() OVER (ORDER BY id) AS new_id
+            FROM agendamentos
+        )
+        UPDATE agendamentos
+        SET id = new_id
+        FROM numbered_agendamentos
+        WHERE agendamentos.id = numbered_agendamentos.id;
+        """)
+        print("IDs dos agendamentos foram renumerados.")
+        conn.commit()
         
-        with open('clientes.json', 'w') as f:
-            f.write(result)
-        print("JSON gerado com sucesso!")
+#----------------------------------------------------------------------------------------
 
+def main():
+    renumber_ids_agendamentos()
+    renumber_ids()
 
-if __name__ == '__main__':
-    generate_json()
+if __name__ == "__main__":
+    main()
