3e6e9cf0b6bc876518db907d4224e6f87ae3ebd5
caveira dev <110509079+Matheushsp@users.noreply.github.com>
2024-11-09 23:28:13 -0300

    vc

--- dentista/app/(tabs)/index.tsx
+++ dentista/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">yzy!</ThemedText>
+        <ThemedText type="title">yzdefdfdffdfdy!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
